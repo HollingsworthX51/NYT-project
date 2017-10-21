9a7eec806fd9c38fd7ab0a0e16efe7486d82eebb
Michael Fessenden <michael.fessenden@gmail.com>
2017-10-21 15:13:53 -0400

    basic formatting of results

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@
 
 
 
-
+var obj;
 
 // main process
 $(document).ready(function() {
@@ -16,54 +16,56 @@ $(document).ready(function() {
         // stop the form from submitting
         event.preventDefault();
 
-
-        console.log("im in the button search");
         var pageLimit = 5;
         var beginDate = "20170101";
         var endDate = "20171231";
-        var query = "politics";
+        var query = $('#searchInput').val();
 
         var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
         queryURL += '?' + $.param({
             'api-key': "c0b4d2e16a014795bbdce9d7e4df8a95",
             'q': query,
             'page': pageLimit,
-            'begin_date': beginDate,
-            'end_date': beginDate
+            // 'begin_date': beginDate,
+            // 'end_date': beginDate
         });
 
         $.ajax({
             url: queryURL,
             method: 'GET',
         }).done(function(response) {
-            console.log(response);
+            // console.log(response);
 
-
-            var results = response.docs;
+            var results = response.response.docs;
             // ========================
+            if (results.length) {
 
-            for (var i = 0; i < results.length; i++) {
-
-
-                var headlineDiv = $("<div>");
-                headlineDiv.attr("id", "headline");
-
-                var headline = results[i].headline.main;
-                var snippet = results[i].docs.snippet;
+                for (var i = 0; i < results.length; i++) {
 
 
-                var p1 = $("<p>");
-                p1 = $("<p>").text("Headline: " + headline);
-                var p2 = $("<p>");
-                p2 = $("<p>").text("Detail: " + snippet);
-
-                headlineDiv.append(p1);
-                headlineDiv.append(p2);
+                    var headlineDiv = $('<div class="card nyt-card">');
+                    headlineDiv.attr("id", "headline");
 
 
-                $("#article").prepend(headlineDiv);
+
+                    var headline = results[i].headline.main;
+                    var snippet = results[i].snippet;
 
 
+                    var p1 = $('<p class="results-healine">Headline: ' + headline + '</p>');
+                    var p2 = $("<p>");
+                    p2 = $("<p>").text("Detail: " + snippet);
+
+                    var bodyDiv = $('<div class="card-body">');
+                    headlineDiv.append(p1);
+                    bodyDiv.append(p2);
+                    headlineDiv.append(bodyDiv);
+
+
+                    $("#articles").prepend(headlineDiv);
+
+
+                }
             }
         }).fail(function(err) {
             throw err;
